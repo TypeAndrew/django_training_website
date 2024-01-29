4a6aeec8726a5b164d768454cb73175b91e7cce6
shulyar <andy.shular@gmail.com>
2024-01-29 18:06:36 +0200

    new project 1

---
+++ landingpagewebsite_root/crm/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from .models import Order
+from .forms import OrderForm
+# Create your views here.
+def first_page(request):
+    object_list = Order.objects.all()
+    form = OrderForm()
+    return render(request, './index.html', {'object_list':object_list,'form': form})
+
+def thanks_page(request):
+    name = request.POST['name']
+    phone = request.POST['phone']
+    element = Order(order_name=name, order_phone = phone)
+    element.save()
+    return render(request,'./thanks_page.html', {'name':name,'phone':phone})
+
