f0cc4a3b338e36104223e443dfbf336cb590af78
shulyar <andy.shular@gmail.com>
2024-02-09 13:48:20 +0200

    2

--- landingpagewebsite_root/cms/admin.py
+++ landingpagewebsite_root/cms/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from .models import CmsSlider
 
 # Register your models here.
-admin.site.register(CmsSlider)
+class CmsAdmin(admin.ModelAdmin):
+    list_display = ('cms_title','cms_text','cms_css','get_img')
+    list_display_links = ('cms_title',)
+    list_editable = ('cms_css',)
+    fields = ('cms_title', 'cms_text', 'cms_css', 'get_img')
+    readonly_fields =  fields = ( 'get_img',)
+    def get_img(self, obj):
+        if obj.cms_img:
+            return mark_safe(f'<img src="{obj.cms_img.url}" width="80px"')
+        else:
+            return 'нема світлини'
+
+    get_img.short_description = 'Мініатюра'
+
+admin.site.register(CmsSlider, CmsAdmin)
--- landingpagewebsite_root/crm/admin.py
+++ landingpagewebsite_root/crm/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
 from .models import Order, StatusCrm, CommentCrm
 # Register your models here.
-admin.site.register(Order)
+class Coment(admin.StackedInline):
+    model = CommentCrm
+    fields = ('coment_dt','coment_text')
+    readonly_fields =('coment_dt',)
+    extra = 0
+
+class OrderAdm(admin.ModelAdmin):
+    list_display = ('id','order_status','order_name','order_phone','order_dt')
+    list_display_links = ('id',  'order_name')
+    search_fields = ('id', 'order_name', 'order_phone', 'order_dt')
+    list_filter = ('order_status',)
+    list_editable = ('order_status','order_phone')
+    list_per_page = 10
+    list_max_show_all = 100
+    fields = ('id', 'order_status', 'order_name', 'order_phone', 'order_dt')
+    readonly_fields = ('id', 'order_dt')
+    inlines = [Coment,]
+admin.site.register(Order,OrderAdm)
 admin.site.register(StatusCrm)
 admin.site.register(CommentCrm)
